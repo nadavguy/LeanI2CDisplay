b45b8193f7896b7c4761843946fbac42c84b0921
raingrey <raingrey@gmail.com>
2020-01-19 13:55:30 +0200

    Added Ch1, Ch2 & Bat measurement

--- LeanDisplayTest.ino
+++ LeanDisplayTest.ino
@@ -18,20 +18,32 @@ unsigned long CurrentTimeuSec = 0;
 unsigned long Fan1DisplayCycle = 0;
 unsigned long Fan2DisplayCycle = 0;
 unsigned long TwentymSecCycle = 0;
+float PreviousVoltageCh1 = 0;
+float PreviousVoltageCh2 = 0;
+float PreviousVoltageBat = 0;
+float RefVoltage = 5.2;
+float Ch1AmpsRefVoltage =507.0;
+
+
 
 
 uint8_t Fan1Phase = 0; 
 uint8_t Fan2Phase = 0; 
 void setup() {
   Serial.begin(9600);
-
+  pinMode(2,OUTPUT);
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A6,INPUT);
+  pinMode(A7,INPUT);
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
     for (;;)
       ; // Don't proceed, loop forevert
   }
-  
+  analogReference(DEFAULT);
   watchdogSetup();
 
   //Test SCreen Will Be removed
@@ -51,8 +63,11 @@ void setup() {
 	// 	display.println("E");
   //   display.display();
   ClearScreen();
-  ClearPixels(Fan1IconXOffset, Fan1IconYOffset, Fan1IconWidth, Fan1IconHeight);
-  drawbitmap(Fan1IconXOffset, Fan1IconYOffset, Fan1IconWidth, Fan1IconHeight, FANOff_bmp);
+  drawText(0,1,"Ch1:");
+  drawText(0,3,"Ch2:");
+  digitalWrite(2, HIGH); 
+  //ClearPixels(Fan1IconXOffset, Fan1IconYOffset, Fan1IconWidth, Fan1IconHeight);
+  //drawbitmap(Fan1IconXOffset, Fan1IconYOffset, Fan1IconWidth, Fan1IconHeight, FANOff_bmp);
   CurrentTimeuSec = micros();
   TwentymSecCycle = micros();
 }
@@ -80,7 +95,9 @@ void loop() {
   }
 
 
-  DisplayFan();
+  DisplayCh1();
+  DisplayCh2();
+  DisplayBat();
 
   // delay(500);
   // ClearPixels(MosfetIconXOffset ,MosfetIconYOffset , MosfetIconWidth, MosfetIconHeight);
@@ -119,6 +136,66 @@ void DisplayFan()
       ClearPixels(Fan1IconXOffset, Fan1IconYOffset, Fan1IconWidth, Fan1IconHeight);
       drawbitmap(Fan1IconXOffset, Fan1IconYOffset, Fan1IconWidth, Fan1IconHeight, FANOff_bmp);
       Fan1DisplayCycle = micros();
+    }
+  }
+}
+
+void DisplayCh1()
+{
+  if ((PreviousVoltageCh1 * 1.01 < float(V1In * RefVoltage / 1023.0)) || (PreviousVoltageCh1 * 0.99 > float(V1In * RefVoltage / 1023.0)))
+  {
+    PreviousVoltageCh1 = float(V1In * RefVoltage / 1023.0);
+    ClearDigits(5, 1, 5);
+    drawNumber(5, 1, float(V1In * RefVoltage / 1023.0));
+    drawText(10,1,"V");
+    ClearDigits(5, 2, 10);
+    drawNumber(5, 2, -1*float( ((Amps1Average - Ch1AmpsRefVoltage)*5.0/1023.0)/0.1 ));
+    drawText(10,2,"A");
+  }
+}
+
+void DisplayCh2()
+{
+  if ((PreviousVoltageCh2 * 1.01 < float(V2In * RefVoltage / 1023.0)) || (PreviousVoltageCh2 * 0.99 > float(V2In * RefVoltage / 1023.0)))
+  {
+    PreviousVoltageCh2 = float(V2In * RefVoltage / 1023.0);
+    ClearDigits(5, 3, 5);
+    drawNumber(5, 3, float(V2In * RefVoltage / 1023.0));
+    drawText(10,3,"V");
+    ClearDigits(5, 4, 10);
+    drawNumber(5, 4, -1*float( ((Amps2Average - Ch1AmpsRefVoltage)*RefVoltage/1023.0)/0.1 ));
+    drawText(10,4,"A");
+  }
+}
+
+void DisplayBat()
+{
+  VBatIn = float(VBatAverage * 5.0 / 1023.0);
+  if ((PreviousVoltageBat * 1.02 < VBatIn) || (PreviousVoltageBat * 0.98 > VBatIn))
+  {
+    Serial.print("VBatIn: ");
+    Serial.println(VBatIn);
+    PreviousVoltageBat = VBatIn;
+    ClearPixels(BatteryIconXOffset, BatteryIconYOffset, BatteryIconWidth, BatteryIconHeight);
+    if (VBatIn > 4.1)
+    {
+      drawbitmap(BatteryIconXOffset, BatteryIconYOffset, BatteryIconWidth, BatteryIconHeight, Battery100Percent_bmp);
+    }
+    else if ((VBatIn <= 4.1) && (VBatIn > 3.9))
+    {
+      drawbitmap(BatteryIconXOffset, BatteryIconYOffset, BatteryIconWidth, BatteryIconHeight, Battery75Percent_bmp);
+    }
+    else if ((VBatIn <= 3.9) && (VBatIn > 3.7))
+    {
+      drawbitmap(BatteryIconXOffset, BatteryIconYOffset, BatteryIconWidth, BatteryIconHeight, Battery50Percent_bmp);
+    }
+    else if ((VBatIn <= 3.7) && (VBatIn > 3.5))
+    {
+      drawbitmap(BatteryIconXOffset, BatteryIconYOffset, BatteryIconWidth, BatteryIconHeight, Battery25Percent_bmp);
+    }
+    else
+    {
+      drawbitmap(BatteryIconXOffset, BatteryIconYOffset, BatteryIconWidth, BatteryIconHeight, Battery0Percent_bmp);
     }
   }
 }
--- MainLogic.h
+++ MainLogic.h
@@ -1,4 +1,6 @@
 #include <avr/wdt.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 volatile unsigned long GeneralCounter = 0;
 bool IsFan1On = false;
@@ -6,6 +8,42 @@ bool IsFan2On = false;
 bool IsPLTR1On = false;
 bool IsPLTR2On = false;
 
+uint16_t V1 = 0;
+uint16_t V1Array[25] = {0};
+uint16_t V1Sum = 0;
+uint8_t V1Counter = 0;
+uint16_t V1Average = 0;
+
+uint16_t Amps1 = 0;
+uint16_t Amps1Array[25] = {0};
+uint16_t Amps1Sum = 0;
+uint8_t Amps1Counter = 0;
+float Amps1Average = 0;
+
+uint16_t V2 = 0;
+uint16_t V2Array[25] = {0};
+uint16_t V2Sum = 0;
+uint8_t V2Counter = 0;
+float V2Average = 0;
+
+uint16_t Amps2 = 0;
+uint16_t Amps2Array[25] = {0};
+uint16_t Amps2Sum = 0;
+uint8_t Amps2Counter = 0;
+float Amps2Average = 0;
+
+uint16_t VBat = 0;
+uint16_t VBatArray[10] = {0};
+uint16_t VBatSum = 0;
+uint8_t VBatCounter = 0;
+float VBatAverage = 0;
+
+float V1In = 0;
+float V2In = 0;
+float VBatIn = 0;
+//unsigned int VA1 = 0;
+//unsigned long VA1In = 0;
+float CalcArrayAverage(uint16_t Array[], uint16_t * Sum,uint8_t * Counter, uint16_t Measuremnt, float ArraySize);
 void watchdogSetup(void)
 {
   cli(); // disable all interrupts
@@ -22,17 +60,83 @@ void watchdogSetup(void)
   // Enter Watchdog Configuration mode:
   WDTCSR |= (1 << WDCE) | (1 << WDE);
   // Set Watchdog settings:
-  WDTCSR = (1 << WDIE) | (0 << WDE) | (0 << WDP3) | (1 << WDP2) | (1 << WDP1) | (1 << WDP0);
+  WDTCSR = (1 << WDIE) | (0 << WDE) | (0 << WDP3) | (1 << WDP2) | (0 << WDP1) | (0 << WDP0);
   sei();
 }
 
 ISR(WDT_vect) 
 {
-    /*Manange PLTR, Fans, Read Temperature measurements */ 
-    Serial.println(GeneralCounter);
-//drawText(6,3,"float(11)");
-/* ReEnable the watchdog interrupt, as this gets reset when entering this ISR and automatically enables the WDE signal that resets the MCU the next time the  timer overflows */
-//WDTCSR = (1 << WDIE) | (1 << WDP2) | (1 << WDP1);
+  //Serial.println(GeneralCounter);
+  //Check Channel1 Voltage & Amper
+  V1 = analogRead(A1); // V1 = Vin * 1.5 / 11.5
+  V1Average = CalcArrayAverage(V1Array, &V1Sum, &V1Counter, V1, 25.0);
+  V1In = V1Average * (9.95+1.52) / 1.52;
+  Serial.print("V1In: ");
+  Serial.println(V1In);
+  Amps1 = analogRead(A2); 
+  Amps1Average = CalcArrayAverage(Amps1Array, &Amps1Sum, &Amps1Counter, Amps1, 25.0);
+  Serial.print("Amps1Average: ");
+  Serial.println(Amps1Average);
 
+  //Check Channel2 Voltage & Amper
+  V2 = analogRead(A7); // V1 = Vin * 1.5 / 11.5
+  V2Average = CalcArrayAverage(V2Array, &V2Sum, &V2Counter, V2, 25.0);
+  V2In = V2Average * (9.95+1.52) / 1.52;
+  Serial.print("V2In: ");
+  Serial.println(V2In);
+  Amps2 = analogRead(A6); 
+  Amps2Average = CalcArrayAverage(Amps2Array, &Amps2Sum, &Amps2Counter, Amps2, 25.0);
+  Serial.print("Amps2Average: ");
+  Serial.println(Amps2Average);
+
+  //Check Battery Voltage 
+  VBat = analogRead(A0); // V1 = Vin * 1.5 / 11.5
+  VBatAverage = CalcArrayAverage(VBatArray, &VBatSum, &VBatCounter, VBat, 10.0);
+  
+  Serial.print("VBat: ");
+  Serial.println(VBatAverage);
 }
 
+
+float CalcArrayAverage(uint16_t Array[], uint16_t * Sum,uint8_t * Counter, uint16_t Measuremnt, float ArraySize)
+{
+  // Serial.println("Calc Average: ");
+  // Serial.print("Sum1: ");
+  *Sum = *Sum - Array[*Counter];
+  // Serial.println(*Sum);
+  Array[*Counter] = Measuremnt;
+  *Sum = *Sum + Measuremnt;
+  // Serial.print("Sum2: ");
+  // Serial.println(*Sum);
+  *Counter=*Counter+1;
+  // Serial.print("Counter: ");
+  // Serial.println(*Counter);
+
+  // Serial.print("Array0: ");
+  // Serial.println(Array[0]);
+  // Serial.print("Array1: ");
+  // Serial.println(Array[1]);
+  // Serial.print("Array2: ");
+  // Serial.println(Array[2]);
+  // Serial.print("Array3: ");
+  // Serial.println(Array[3]);
+  // Serial.print("Array4: ");
+  // Serial.println(Array[4]);
+  // Serial.print("Array5: ");
+  // Serial.println(Array[5]);
+  // Serial.print("Array6: ");
+  // Serial.println(Array[6]);
+  // Serial.print("Array7: ");
+  // Serial.println(Array[7]);
+  // Serial.print("Array8: ");
+  // Serial.println(Array[8]);
+  // Serial.print("Array9: ");
+  // Serial.println(Array[9]);
+
+
+  if (*Counter >= ArraySize)
+  {
+    *Counter = 0;
+  }
+  return *Sum / ArraySize;
+}
